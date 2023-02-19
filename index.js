b22f8c553a06e02797acaadbee4e7df7950f44d7
Parth Sanjaykumar Vaghela <vaghela.sanjaykumar@publicissapient.com>
2023-02-19 16:54:17 +0530

    user creating completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 // Database connection requirements.
 connectToMongo();
 const app = express();
-const port = 3000;
+const port = 5000;
 app.use(express.json());
 
 // Default Path/Homepage/Landing.
--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+// Json web secret key(Ideally this shouldn't be here, I guess)
+const JSW_SECRET = "iamsherlockvaghela";
 
 // Creating post request to save a user locally into the db
-router.post('/', [
-    body('firstName','Firstname should be atleast 3 characters, please enter again.').isLength({min: 3}),
-    body('lastName','Lastname should be atleast 3 characters, please enter again.').isLength({min: 3}),
-    body('email','Please enter a vailid email id.').isEmail(),
-    body('password','Password must be atleast 5 characters long. Please re-enter.').isLength({min: 5}),
+router.post(
+  "/createUser",
+  [
+    body(
+      "firstName",
+      "Firstname should be atleast 3 characters, please enter again."
+    ).isLength({ min: 3 }),
+    body(
+      "lastName",
+      "Lastname should be atleast 3 characters, please enter again."
+    ).isLength({ min: 3 }),
+    body("email", "Please enter a vailid email id.").isEmail(),
+    body(
+      "password",
+      "Password must be atleast 5 characters long. Please re-enter."
+    ).isLength({ min: 5 }),
     // add another validation for phone number
-] , (req,res) => {
+  ],
+  async (req, res) => {
+    // returning errors when found.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    try {
+      // check if user exists already
+      let userEmail = await User.findOne({email: req.body.email});
+      let userPhoneNumber = await User.findOne({email: req.body.phoneNumber});
+      if(userEmail || userPhoneNumber){
+        return res.status(404).json({error : 'User with this email or phone number already exists!!!'});
+      }
+      // bcrypt for password protection
+      const salt = await bcrypt.genSalt();
+      const securePassword = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
+        password: securePassword,
         phoneNumber: req.body.phoneNumber,
-      }).then(user => res.json(user))
-      .catch(err => {
-        console.log(err);
-        res.json({error: 'Please enter a unique value for email and mobile number.'});
       });
-});
+      // Instead of sending a ok message I will try and send a web token so that 
+      // when the user logs in for the next time he/she can be verified.
+      const data = {
+        user: {
+          id: user.id
+        }
+      };
+      const authToken = jwt.sign(data, JSW_SECRET);
+      res.send(authToken);
+    } catch(error) {
+      console.error(error.message);
+      res.status(500).send("Something went wrong!!!");
+    }
+  }
+);
 
 module.exports = router;
