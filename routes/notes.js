395972c66d1634b67f01ec779e879826fd54ce73
Parth Sanjaykumar Vaghela <vaghela.sanjaykumar@publicissapient.com>
2023-02-20 10:08:01 +0530

    Note updation completed

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,102 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
+const fetchUser = require("../middlewares/fetchuser");
 
-router.get('/', (req,res) => {
-    obj = {
-        name: 'parth vaghela',
-        age: 23,
-        date: '18th Feb 2023',
+// Fetching all the notes, login required
+router.get("/getnotes", fetchUser, async (req, res) => {
+  try {
+    user = req.user;
+    const allNotes = await notes.find({ user: req.user.id });
+    res.json(allNotes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Something went wrong!!!");
+  }
+});
+
+// Adding new note using post, login required
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    body("title", "Title should be atleast 3 characters long").isLength({
+      min: 3,
+    }),
+    body(
+      "description",
+      "Description should be atleast 5 characters long."
+    ).isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    // returning errors when found.
+    const noteTitle = req.body.title;
+    const noteDescription = req.body.description;
+    const noteTags = req.body.tags;
+    console.log(noteTitle, noteDescription, noteTags);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+
+    try {
+      const newNote = new notes({
+        title: noteTitle,
+        description: noteDescription,
+        tags: noteTags,
+        user: req.user.id,
+      });
+      const savedNote = await newNote.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Something went wrong!!!");
+    }
+  }
+);
+
+// Updating an existing note
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  try {
+    console.log(req.params);
+    // returning errors when found.
+    const { title, description, tags } = req.body;
+    const noteId = req.params.id;
+
+    let updatedNote = {};
+    if (title) {
+      updatedNote.title = title;
+    }
+    if (description) {
+      updatedNote.description = description;
+    }
+    if (tags) {
+      updatedNote.tags = tags;
+    }
+
+    // Find if the note to be updated exists and check if it exists
+    let receivedNote = await notes.findById(noteId);
+    if (!receivedNote) {
+      return res.status(400).send("Not found");
+    }
+
+    // Check if the note belongs to the correct user or not.
+    if (receivedNote.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    updatedNote = await notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: updatedNote },
+      { new: true }
+    );
+    res.json(updatedNote);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Something went wrong!!!");
+  }
 });
 
 module.exports = router;
